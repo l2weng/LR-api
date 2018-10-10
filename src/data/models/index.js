13b3e1d24722f63760871bf4ed604cec70e69577
Louis Weng <louis396@hotmail.com>
2018-10-10 16:12:05 +0800

    commit table relationship

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -7,15 +7,18 @@ import AuthConfig from './AuthConfig';
 import DataSet from './DataSet';
 import Project from './Project';
 import Sku from './Sku';
+import UserProjects from './UserProjects';
+import Invitation from './Invitation';
+import Reference from './Reference';
 
 /**
- * Team has many user
+ * Team N to N user
  */
 Team.hasMany(User, { foreignKey: 'teamId', as: 'Workers' });
 User.belongsTo(Team, { foreignKey: 'teamId', as: 'Team' });
 
 /**
- * One user has one membership
+ *  User 1 to 1 membership
  */
 Membership.belongsTo(User, { foreignKey: 'userId' });
 
@@ -37,81 +40,28 @@ AuthConfig.belongsTo(MembershipConfig, {
 DataSet.belongsTo(Project, { foreignKey: 'projectId' });
 
 /**
- * Project 1 to many sku
+ * Project 1 to N sku
  */
 Project.hasMany(Sku, { foreignKey: 'projectId', as: 'Skus' });
 Sku.belongsTo(Project, { foreignKey: 'projectId', as: 'Project' });
 
-User.hasMany(Project, { foreignKey: 'projectId', as: 'Skus' });
-Sku.belongsTo(Project, { foreignKey: 'projectId', as: 'Project' });
+/**
+ * Sku 1 to N reference
+ */
+Sku.hasMany(Reference, { foreignKey: 'skuId', as: 'References' });
+Reference.belongsTo(Sku, { foreignKey: 'skuId', as: 'Sku' });
 
-Sku.hasMany(Invitation, {
-  foreignKey: 'windFieldId',
-  as: 'fieldMachines',
-  onUpdate: 'cascade',
-});
-Invitation.belongsTo(Sku, { foreignKey: 'windFieldId', as: 'field' });
+/**
+ * User N to N projects
+ */
+User.belongsToMany(Project, { through: UserProjects });
+Project.belongsToMany(User, { through: UserProjects });
 
-Reference.hasMany(Invitation, {
-  foreignKey: 'turbineConfigId',
-  as: 'turbineConfigTurbine',
-  onUpdate: 'cascade',
-});
-Invitation.belongsTo(Reference, {
-  foreignKey: 'turbineConfigId',
-  as: 'turbineConfig',
-});
-
-Invitation.hasMany(Feedback, {
-  foreignKey: 'windMachineId',
-  as: 'machineVanes',
-  onUpdate: 'cascade',
-});
-Feedback.belongsTo(Invitation, { foreignKey: 'windMachineId', as: 'machine' });
-
-Feedback.hasMany(Task, {
-  foreignKey: 'windVaneId',
-  as: 'vaneFanPhotos',
-  onUpdate: 'cascade',
-});
-Task.belongsTo(Feedback, { foreignKey: 'windVaneId', as: 'vane' });
-
-Invitation.hasMany(RoutingInspect, {
-  foreignKey: 'windMachineId',
-  as: 'machineRoutingInspects',
-  onUpdate: 'cascade',
-});
-RoutingInspect.belongsTo(Invitation, {
-  foreignKey: 'windMachineId',
-  as: 'machine',
-});
-
-RoutingInspect.hasMany(Task, {
-  foreignKey: 'routingInspectId',
-  as: 'routingInspectsPhotos',
-  onUpdate: 'cascade',
-});
-Task.belongsTo(RoutingInspect, {
-  foreignKey: 'routingInspectId',
-  as: 'routingInspect',
-});
-
-Task.belongsToMany(Photo, {
-  through: {
-    model: UserLogin,
-    unique: false,
-  },
-  foreignKey: 'fanPhotoId',
-  constraints: false,
-});
-Photo.belongsToMany(Task, {
-  through: {
-    model: UserLogin,
-    unique: false,
-  },
-  foreignKey: 'defectTypeId',
-  constraints: false,
-});
+/**
+ * User 1 to N invitation
+ */
+User.hasMany(Invitation, { foreignKey: 'userId', as: 'Invitations' });
+Invitation.belongsTo(User, { foreignKey: 'userId', as: 'User' });
 
 function sync(...args) {
   return sequelize.sync(...args);
