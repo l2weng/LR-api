e37c2da848ef2af8fb9a43bcc0ad0e04ffc5c0cd
Louis Weng <louis396@hotmail.com>
2018-10-16 23:49:42 +0800

    Invitation

--- src/data/models/Invitation.js
+++ src/data/models/Invitation.js
@@ -1,15 +1,18 @@
 import { DataTypes } from 'sequelize';
 import Model from '../sequelize';
 // 邀请
-const Invitation = Model.define('WindMachine', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
+const Invitation = Model.define('Invitation', {
+  invitationId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV1,
     primaryKey: true,
   },
 
   url: { type: DataTypes.STRING },
-  active: { type: DataTypes.BOOLEAN },
+  /**
+   * 邀请信状态 0:未使用, 1: 已激活, 2: 过期
+   */
+  status: { type: DataTypes.INTEGER },
 });
 
 export default Invitation;
