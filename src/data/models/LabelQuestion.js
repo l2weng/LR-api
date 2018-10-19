0b0edfd4e697ea884e03aa60918865f9a03191b0
Louis Weng <louis396@hotmail.com>
2018-10-19 15:34:24 +0800

    label question reference

--- src/data/models/LabelQuestion.js
+++ src/data/models/LabelQuestion.js
@@ -5,25 +5,25 @@ import Model from '../sequelize';
  * Project type is Label question
  */
 const LabelQuestion = Model.define('LabelQuestion', {
-    labelQuestionId: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV1,
-        primaryKey: true,
-    },
+  labelQuestionId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV1,
+    primaryKey: true,
+  },
 
-    title: { type: DataTypes.STRING },
-    /**
-     *  问题保存用的key
-     */
-    key: { type: DataTypes.STRING },
-    /**
-     *  true answer 保存的客户需要的value形式, e.g. 'Yes' '0'
-     */
-    trueAnswerVal: { type: DataTypes.STRING },
-    /**
-     *  false answer 保存的客户需要的value形式, e.g. 'NO' '1'
-     */
-    falseAnswerVal: { type: DataTypes.STRING },
+  title: { type: DataTypes.STRING },
+  /**
+   *  问题保存用的key
+   */
+  key: { type: DataTypes.STRING },
+  /**
+   *  true answer 保存的客户需要的value形式, e.g. 'Yes' '0'
+   */
+  trueAnswerVal: { type: DataTypes.STRING },
+  /**
+   *  false answer 保存的客户需要的value形式, e.g. 'NO' '1'
+   */
+  falseAnswerVal: { type: DataTypes.STRING },
 });
 
 export default LabelQuestion;
