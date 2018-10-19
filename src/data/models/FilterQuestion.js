ca6986c718d570f95b5fa9594f732fa02e1b4ace
Louis Weng <louis396@hotmail.com>
2018-10-19 11:18:53 +0800

    Level photo project sku reference

--- src/data/models/FilterQuestion.js
+++ src/data/models/FilterQuestion.js
@@ -5,14 +5,13 @@ import Model from '../sequelize';
  * Project type true false question
  */
 const FilterQuestion = Model.define('FilterQuestion', {
-  questionId: {
+  filterQuestionId: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV1,
     primaryKey: true,
   },
 
   title: { type: DataTypes.STRING },
-  key: { type: DataTypes.STRING },
   active: { type: DataTypes.INTEGER },
 });
 
