a12c97108857f3510f3ab63b80b01aa48a373b21
Louis Weng <louis396@hotmail.com>
2019-01-07 16:02:45 +0800

    yes yes company

--- src/data/dataUtils.js
+++ src/data/dataUtils.js
@@ -1,38 +1,71 @@
-const userTypeDesc = { '0': 'individual', '1': 'enterprise' };
-const userType = { individual: '0', enterprise: '1' };
+const userTypeDesc = {'0': 'individual', '1': 'enterprise'}
+const userType = {individual: '0', enterprise: '1'}
 
 // 0:激活状态, 1: 未激活, 2, 冻结
 // const status = { active: 0, inactive: 1, locked: 2 };
-const statusDesc = { 0: 'inactive', 1: 'active', 2: 'locked' };
+const statusDesc = {0: 'inactive', 1: 'active', 2: 'locked'}
 
-// const codeMessage = {
-//   200: '服务器成功返回请求的数据。',
-//   201: '新建或修改数据成功。',
-//   202: '一个请求已经进入后台排队（异步任务）。',
-//   204: '删除数据成功。',
-//   400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-//   401: '用户没有权限（令牌、用户名、密码错误）。',
-//   403: '用户得到授权，但是访问是被禁止的。',
-//   404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-//   406: '请求的格式不可得。',
-//   410: '请求的资源被永久删除，且不会再得到的。',
-//   422: '当创建一个对象时，发生一个验证错误。',
-//   500: '服务器发生错误，请检查服务器。',
-//   502: '网关错误。',
-//   503: '服务不可用，服务器暂时过载或维护。',
-//   504: '网关超时。',
-// };
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+}
+const enCodeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+}
 
-const resMessage = { 0: 'success', 1: 'failure' };
-const optTypes = { 0: 'create', 1: 'update', 3: 'read', 4: 'delete' };
+function getMessageByCode (code = 200, lang = 'cn') {
+  return lang === "en" ? enCodeMessage[code] : codeMessage[code]
+}
 
-function resBuild(obj = null, resType = 0, optType = 0, message = '') {
+const resMessage = {0: 'success', 1: 'failure'}
+const optTypes = {0: 'create', 1: 'update', 3: 'read', 4: 'delete'}
+
+function resBuild (obj = null, resType = 0, optType = 0, message = '') {
   return {
     result: resMessage[resType],
     message: `${optTypes[optType]} ${obj.constructor.name} ${message}`,
     model: obj !== null ? obj.constructor.name : '',
     obj,
-  };
+  }
 }
 
-export { resBuild, statusDesc, userTypeDesc, userType };
+function validateEmail (email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
+}
+
+export {
+  resBuild,
+  statusDesc,
+  userTypeDesc,
+  userType,
+  validateEmail,
+  getMessageByCode,
+}
--- src/routes/labelreal/users.js
+++ src/routes/labelreal/users.js
@@ -1,34 +1,57 @@
-import User from '../../data/models/User';
+import User from '../../data/models/User'
+import Company from '../../data/models/Company'
 import {
   statusDesc,
   userTypeDesc,
   resBuild,
-  // userType as _userType,
-} from '../../data/dataUtils';
-import express from 'express';
+  userType as _userType,
+  validateEmail, getMessageByCode,
+} from '../../data/dataUtils'
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/create', (req, res) => {
-  const { userType, status } = req.body;
-  const userObj = {
+  const {userType, status, email, companyName} = req.body
+  let userObj = {
     userTypeDesc: userTypeDesc[userType],
     statusDesc: statusDesc[status],
     ...req.body,
-  };
-  // if (_userType.enterprise === userType) {
-  // }
-
-  User.create(userObj)
-    .then(user => {
-      delete user.dataValues.password;
-      delete user.dataValues.password_hash;
-      res.json(resBuild(user));
+  }
+  let userSave = function () {
+    User.create(userObj).then(user => {
+      delete user.dataValues.password
+      delete user.dataValues.password_hash
+      res.json(resBuild(user))
+    }).catch(err => {
+      res.status(500).send(err)
     })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
+  }
+  if (_userType.enterprise === userType) {
+    if (validateEmail(email)) {
+      let emailDomain = email.substring(email.lastIndexOf('@') + 1)
+      Company.findOne({
+        where: {emailDomain},
+      }).then(company => {
+        if (company !== null) {
+          userObj = {companyId: company.companyId, ...userObj}
+          userSave()
+        } else {
+          Company.create({name: companyName, emailDomain}).then(company => {
+            userObj = {companyId: company.companyId, ...userObj}
+            userSave()
+          }).catch(err => {
+            res.status(500).send(err)
+          })
+        }
+      })
+    } else {
+      res.status(400).send(getMessageByCode(400))
+    }
+  } else {
+    userSave()
+  }
+})
 //
 // router.post('/update', (req, res) => {
 //   const userUpdateInfo = async done => {
@@ -62,4 +85,4 @@ router.post('/create', (req, res) => {
 //   });
 // });
 
-export default router;
+export default router
