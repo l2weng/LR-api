f3a34850baebcbbeb12349bb7b4eee52d2ab5b43
Louis Weng <louis396@hotmail.com>
2019-01-07 16:43:18 +0800

    update users routers

--- src/routes/labelreal/users.js
+++ src/routes/labelreal/users.js
@@ -52,37 +52,5 @@ router.post('/create', (req, res) => {
     userSave()
   }
 })
-//
-// router.post('/update', (req, res) => {
-//   const userUpdateInfo = async done => {
-//     const userUpdateResult = await WindUser.update(
-//       {
-//         name: req.body.name && req.body.name.trim(),
-//         displayName: req.body.displayName && req.body.displayName.trim(),
-//         type: req.body.type,
-//         typeDes: req.body.type && windUserDes[req.body.type],
-//         phone: req.body.phone && req.body.phone.trim(),
-//         email: req.body.email && req.body.email.trim(),
-//       },
-//       {
-//         where: { userId: req.body.userId },
-//       },
-//     )
-//       .then(result => ({
-//         result: 'success',
-//         obj: result,
-//       }))
-//       .catch(err => ({ result: 'error', msg: err.name }));
-//     done(userUpdateResult);
-//   };
-//
-//   userUpdateInfo(data => {
-//     if (data.result === 'success') {
-//       res.status(200).send({ result: 'success', msg: 'success' });
-//     } else if (data.result === 'error') {
-//       res.status(500).send(data);
-//     }
-//   });
-// });
 
 export default router
