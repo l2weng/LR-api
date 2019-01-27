90fbdaf63c5fb9900a5f9a3c11b951818bbb36c3
Louis Weng <louis396@hotmail.com>
2019-01-27 18:17:44 +0800

    add user team support

--- src/routes/labelreal/teams.js
+++ src/routes/labelreal/teams.js
@@ -1,4 +1,5 @@
 import Team from '../../data/models/Team'
+import User from '../../data/models/User'
 import {
   resBuild,
 } from '../../data/dataUtils'
@@ -8,7 +9,10 @@ const router = express.Router()
 
 router.post('/create', (req, res) => {
   Team.create(req.body).then(team => {
-    res.json(resBuild(team))
+    User.findById(req.body.userId).then(user => {
+      user.addTeam(team, { through: { isOwner: true }})
+      res.json(resBuild(team))
+    })
   }).catch(err => {
     res.status(500).send(err)
   })
