d049883957be73cc3e39d6b7738bdcd872252b26
Louis Weng <louis396@hotmail.com>
2019-03-28 21:50:12 +0800

    update task photo

--- src/routes/labelreal/photos.js
+++ src/routes/labelreal/photos.js
@@ -9,18 +9,25 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/syncPhoto', (req, res) => {
-  const {taskId} = req.body
+  const {taskId, photoId} = req.body
   return Task.findById(taskId).then(task => {
-    return Photo.upsert({
-      ...req.body,
-    }).then(photo => {
-      task.addPhoto(photo)
-      res.json(resBuild(photo))
+    return Photo.findById(photoId).then(photo => {
+      if (photo) {
+        return photo.update({...req.body}).then(photo => {
+          task.addPhoto(photo)
+          res.json(resBuild(photo))
+        })
+      }
+      else {
+        return Photo.create({...req.body}).then(photo => {
+          task.addPhoto(photo)
+          res.json(resBuild(photo))
+        })
+      }
     })
   }).catch(err => {
     resErrorBuild(res, 500, err)
   })
 })
-
 
 export default router
