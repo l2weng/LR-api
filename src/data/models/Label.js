adc37f47afac1ae3b74a76bf8444eda7db009757
Louis Weng <louis396@hotmail.com>
2019-04-01 15:48:45 +0800

    Add properties into label model

--- src/data/models/Label.js
+++ src/data/models/Label.js
@@ -3,19 +3,23 @@ import Model from '../sequelize';
 
 const Label = Model.define('Label', {
   labelId: {
-    type: DataTypes.FLOAT,
-    autoIncrement: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV1,
     primaryKey: true,
   },
 
   x: { type: DataTypes.INTEGER, defaultValue: 0 },
   y: { type: DataTypes.INTEGER, defaultValue: 0 },
+  angle: { type: DataTypes.INTEGER, defaultValue: 0 },
+  mirror: { type: DataTypes.BOOLEAN, defaultValue: false},
+  width: {type:DataTypes.INTEGER, defaultValue: 0 },
+  height: {type:DataTypes.INTEGER, defaultValue: 0 },
   /**
    * label type, 0: 矩形框, 1:......
    */
   type: { type: DataTypes.INTEGER },
   /**
-   * 0: new, 1: updated, removed
+   * 0: new, 1: updated, 2: removed
    */
   status: { type: DataTypes.INTEGER}
 });
--- src/data/types/LabelType.js
+++ src/data/types/LabelType.js
@@ -2,20 +2,21 @@ import {
   GraphQLObjectType as ObjectType,
   GraphQLInt as IntType,
   GraphQLFloat as FloatType,
+  GraphQLBoolean as BooleanType,
 } from 'graphql';
 
 const LabelType = new ObjectType({
   name: 'labelStory',
   fields: {
-    labelId: { type: FloatType },
+    labelId: { type: StringType },
     x: { type: IntType },
     y: { type: IntType },
-    /**
-     * label type, 0: 矩形框, 1:......
-     */
+    angle: { type: IntType },
+    mirror: { type: BooleanType },
+    width: { type: IntType },
+    height: { type: IntType },
     type: { type: IntType },
     status: { type: IntType}
-
   },
 });
 
