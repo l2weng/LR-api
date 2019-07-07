d704cf675c0a93be7e9228bae6b24fd6a5e36ab8
Louis Weng <louis396@hotmail.com>
2019-07-07 21:54:50 +0800

    skip label

--- src/routes/labelreal/labels.js
+++ src/routes/labelreal/labels.js
@@ -1,87 +1,100 @@
-import Label from '../../data/models/Label';
-import Task from '../../data/models/Task';
-import Photo from '../../data/models/Photo';
-import { labelStatus } from '../../data/dataUtils';
-import { resBuild, resErrorBuild } from '../../data/dataUtils';
-import express from 'express';
-import sequelize from '../../data/sequelize';
+import Label from '../../data/models/Label'
+import Task from '../../data/models/Task'
+import Photo from '../../data/models/Photo'
+import { labelStatus } from '../../data/dataUtils'
+import { resBuild, resErrorBuild } from '../../data/dataUtils'
+import express from 'express'
+import sequelize from '../../data/sequelize'
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/saveLabels', (req, res) => {
-  const { labels, photoId, myTaskId } = req.body;
-  const updatedTime = Date.now();
+  const {labels, photoId, myTaskId} = req.body
+  const updatedTime = Date.now()
   for (let i = 0; i < labels.length; i++) {
-    labels[i].updatedTime = updatedTime;
+    labels[i].updatedTime = updatedTime
   }
-  return sequelize
-    .transaction(t =>
-      Photo.findById(photoId)
-        .then(
-          photo =>
-            photo.getTasks().then(tasks => {
-              tasks.map(task => {
-                if (task.taskId === myTaskId) {
-                  task.TaskPhotos.photoStatus = labelStatus.submitted;
-                  task.TaskPhotos.updatedTime = updatedTime;
-                  task.TaskPhotos.projectId = task.projectId;
-                  return task.TaskPhotos.save();
-                }
-              });
-            }),
-          { transaction: t },
-        )
-        .then(photo =>
-          Label.bulkCreate(
-            labels,
-            {
-              returning: true,
-              updateOnDuplicate: [
-                'x',
-                'y',
-                'angle',
-                'mirror',
-                'width',
-                'height',
-                'updatedTime',
-                'id',
-              ],
-            },
-            { transaction: t },
-          ),
-        ),
-    )
-    .then(result => {
-      res.json(resBuild(result));
-      // Transaction has been committed
-    })
-    .catch(err => {
-      resErrorBuild(res, 500, err);
-      // Transaction has been rolled back
-    });
-});
+  return sequelize.transaction(t =>
+    Photo.findById(photoId).then(
+      photo =>
+        photo.getTasks().then(tasks => {
+          tasks.map(task => {
+            if (task.taskId === myTaskId) {
+              task.TaskPhotos.photoStatus = labelStatus.submitted
+              task.TaskPhotos.updatedTime = updatedTime
+              task.TaskPhotos.projectId = task.projectId
+              return task.TaskPhotos.save()
+            }
+          })
+        }),
+      {transaction: t},
+    ).then(photo =>
+      Label.bulkCreate(
+        labels,
+        {
+          returning: true,
+          updateOnDuplicate: [
+            'x',
+            'y',
+            'angle',
+            'mirror',
+            'width',
+            'height',
+            'updatedTime',
+            'id',
+          ],
+        },
+        {transaction: t},
+      ),
+    ),
+  ).then(result => {
+    res.json(resBuild(result))
+    // Transaction has been committed
+  }).catch(err => {
+    resErrorBuild(res, 500, err)
+    // Transaction has been rolled back
+  })
+})
 
 router.post('/queryLabels', (req, res) => {
-  const { taskId } = req.body;
+  const {taskId} = req.body
   return Task.findById(taskId).then(task =>
-    task
-      .getPhotos({
-        include: [
-          {
-            model: Label,
-            required: true,
-            as: 'Labels',
-          },
-        ],
-      })
-      .then(photos => {
-        res.json(photos);
-      })
-      .catch(err => {
-        console.log(err);
-        resErrorBuild(res, 500, err);
-      }),
-  );
-});
+    task.getPhotos({
+      include: [
+        {
+          model: Label,
+          required: true,
+          as: 'Labels',
+        },
+      ],
+    }).then(photos => {
+      res.json(photos)
+    }).catch(err => {
+      console.log(err)
+      resErrorBuild(res, 500, err)
+    }),
+  )
+})
 
-export default router;
+router.post('/skipLabel', (req, res) => {
+  const {photoId, myTaskId} = req.body
+  const updatedTime = Date.now()
+  return Photo.findById(photoId).then(
+    photo =>
+      photo.getTasks().then(tasks => {
+        tasks.map(task => {
+          if (task.taskId === myTaskId) {
+            task.TaskPhotos.photoStatus = labelStatus.skipped
+            task.TaskPhotos.updatedTime = updatedTime
+            task.TaskPhotos.projectId = task.projectId
+            task.TaskPhotos.save()
+            res.json(photo)
+          }
+        })
+      })
+  ).catch(err => {
+    resErrorBuild(res, 500, err)
+  })
+})
+
+export default router
